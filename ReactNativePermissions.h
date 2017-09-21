d555cce2579d23589b044e38164c188885b07bd4
Adam Grayson <applead@aim.com>
2017-09-21 09:19:31 -0400

    updated gitignore, updated ReactNativePermissions.h to solve conflict with scrappulous repo

--- ReactNativePermissions.h
+++ ReactNativePermissions.h
@@ -6,13 +6,16 @@
 //  Copyright © 2016 Yonah Forst. All rights reserved.
 //
 
-#if __has_include("RCTBridgeModule.h")
-  #import "RCTBridgeModule.h"
-#else
-  #import <React/RCTBridgeModule.h>
-#endif
+//#if __has_include("RCTBridgeModule.h")
+//  #import "RCTBridgeModule.h"
+//#else
+//  #import <React/RCTBridgeModule.h>
+//#endif
+
+// See https://github.com/facebook/react-native/issues/15775#issuecomment-327082159
+
+#import <React/RCTBridgeModule.h>
 
 @interface ReactNativePermissions : NSObject <RCTBridgeModule>
-
 
 @end
